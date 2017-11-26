0b5df1d8742f8de7d3e47880236cd7473a803680
joshlgrossman <josh.l.grossman@gmail.com>
2017-11-26 12:08:44 -0600

    split request logic into util func

--- src/client/utils/loginUser.js
+++ src/client/utils/loginUser.js
@@ -2,5 +2,5 @@ import { post } from 'axios';
 
 export function loginUser({ username, password }) {
   // return post('https://jsonplaceholder.typicode.com/post', { username, password });
-  return Promise.resolve({ name: 'Josh ' });
+  return Promise.resolve({ name: 'Josh' });
 }
