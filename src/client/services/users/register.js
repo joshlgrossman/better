33ce808b5d170c724b31666fac079c85cd744dc7
joshlgrossman <josh.l.grossman@gmail.com>
2017-12-03 13:52:45 -0600

    map response to data

--- src/client/services/users/register.js
+++ src/client/services/users/register.js
@@ -1,5 +1,5 @@
 import { post } from 'axios';
 
 export function register({ username, password }) {
-  return post(`/users`, { username, password });
+  return post(`/users`, { username, password }).then(response => response.data);
 }
