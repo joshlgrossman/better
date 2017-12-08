3f804ff39e28e20d40813dca03c6edcf1057da5e
joshlgrossman <josh.l.grossman@gmail.com>
2017-12-07 20:41:13 -0600

    export actions

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -4,6 +4,9 @@ import * as Header from '../components/header/actions';
 import { setUser } from './setUser';
 import { setLocation } from './setLocation';
 
+export * from './setUser';
+export * from './setLocation';
+
 export const actions = {
   setUser,
   setLocation,
