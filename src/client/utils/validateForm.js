71485ffe813592d52accd2d55cdc971c3accf050
joshlgrossman <josh.l.grossman@gmail.com>
2017-12-07 21:41:21 -0600

    move eslint ignore

--- src/client/utils/validateForm.js
+++ src/client/utils/validateForm.js
@@ -5,10 +5,10 @@ export function validateForm(schema) {
         schema[key].check(value, state.data) ? errors : [...errors, { message: schema[key].message, value }],
       []
     );
+    /* eslint eqeqeq:"off" */
     return {
       ...state,
       isValid: !invalidFields.length,
-      /* eslint eqeqeq:"off" */
       errors: invalidFields.filter(f => f.value != null).map(f => f.message)
     };
   };
