046a262842647f724a8dc29e3886027dcc4e4368
joshlgrossman <josh.l.grossman@gmail.com>
2017-12-07 18:09:39 -0600

    form validator action factory

---
+++ src/client/utils/validate.js
@@ -0,0 +1,15 @@
+export function validate(schema) {
+  return state => {
+    const invalidFields = Object.entries(state.data).reduce(
+      (errors, [key, value]) =>
+        schema[key].check(value, state.data) ? errors : [...errors, { message: schema[key].message, value }],
+      []
+    );
+    return {
+      ...state,
+      isValid: !invalidFields.length,
+      /* eslint eqeqeq:"off" */
+      errors: invalidFields.filter(f => f.value != null).map(f => f.message)
+    };
+  };
+}
