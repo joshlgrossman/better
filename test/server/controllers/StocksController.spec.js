5cada7f7c86647d10df93428141b2429bdd21886
joshlgrossman <josh.l.grossman@gmail.com>
2018-01-06 22:18:58 -0600

    stocks controller unit tests

---
+++ test/server/controllers/StocksController.spec.js
@@ -0,0 +1,66 @@
+import { expect, use } from 'chai';
+import { stub, match } from 'sinon';
+import * as sinonChai from 'sinon-chai';
+import { StocksController } from '../../../src/server/controllers';
+
+use(sinonChai);
+
+describe('server.controllers.StocksController', () => {
+  
+  let stocksService;
+  let stocksAssembler;
+  let controller;
+  let resp;
+
+  beforeEach(() => {
+    stocksService = {
+      get: stub().callsFake(() => Promise.resolve({ get: 'test' })),
+      list: stub().callsFake(() => Promise.resolve([{ list: 'test' }]))
+    };
+
+    stocksAssembler = {
+      assemble: stub().returns('assembled')
+    };
+
+    controller = new StocksController(stocksService, stocksAssembler);
+  });
+
+  describe('when getting a stock', () => {
+    beforeEach(async () => {
+      resp = await controller.get({ symbol: 'TEST' });
+    });
+    
+    it('should query for the transactions', () => {
+      expect(stocksService.get).to.have.been.calledWith('TEST');
+    });
+    
+    it('should assemble the results', () => {
+      expect(stocksAssembler.assemble).to.have.been.calledWith(
+        match({ get: 'test' }));
+    });
+
+    it('should return the assembled results', () => {
+      expect(resp).to.deep.equal('assembled');
+    });
+  });
+
+  describe('when listing stocks', () => {
+    beforeEach(async () => {
+      resp = await controller.list();
+    });
+    
+    it('should query for the transactions', () => {
+      expect(stocksService.list).to.have.been.called;
+    });
+    
+    it('should assemble the results', () => {
+      expect(stocksAssembler.assemble).to.have.been.calledWith(
+        match({ list: 'test' }));
+    });
+
+    it('should return the assembled results', () => {
+      expect(resp).to.deep.equal(['assembled']);
+    });
+  });
+
+});
