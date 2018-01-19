1efcb62174ff9273df4956b2863fb10972ff5a3e
joshlgrossman <josh.l.grossman@gmail.com>
2018-01-19 16:33:05 -0600

    loader component

---
+++ src/client/components/loader/view.jsx
@@ -0,0 +1,14 @@
+import { h } from 'hyperapp';
+
+import * as styles from './styles.less';
+
+export const Loader = ({ input }) => (
+  <div {...styles}>
+    <div className={`loader ${ input.isVisible ? 'shown' : 'hidden' }`}>
+      <div className="dot dot-one" />
+      <div className="dot dot-two" />
+      <div className="dot dot-three" />
+      <div className="dot dot-four" />
+    </div>
+  </div>
+);
