ce97ce486b574085ed5672925a1440512042c05d
joshlgrossman <josh.l.grossman@gmail.com>
2018-02-08 17:18:50 -0600

    fix spacing

--- src/client/components/loader/view.jsx
+++ src/client/components/loader/view.jsx
@@ -4,7 +4,7 @@ import * as styles from './styles.less';
 
 export const Loader = ({ input }) => (
   <div {...styles}>
-    <div className={`loader ${ input.isVisible ? 'shown' : 'hidden' }`}>
+    <div className={`loader ${input.isVisible ? 'shown' : 'hidden'}`}>
       <div className="dot dot-one" />
       <div className="dot dot-two" />
       <div className="dot dot-three" />
